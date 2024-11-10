460d58a5e04860f0c007bec3d4ba115a4aa7dead
matyas94 <marton.matyas@students.jedlik.eu>
2024-11-10 20:08:24 +0100

    majdnem

--- rulette.py
+++ rulette.py
@@ -4,14 +4,7 @@ import time
 
 
 egyenleg = 20
-print(egyenleg)
-bet = int(input("Mennyit szeretnél rakni? "))
 
-while bet > egyenleg:
-    print("Nem tudsz ennyit rakni.")
-    bet = int(input("Mennyit szeretnél rakni? "))
-
-egyenleg -= bet
 
 
 if 1 == 1:
@@ -116,6 +109,7 @@ if 1 == 1:
     rr46 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
     rr47 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
     rr48 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
+print(f"Egyenleg: {egyenleg}")
 print(f"""
 |--------------|
 |       {r0}      |
@@ -174,166 +168,118 @@ if help == "help":
 [1ST COL]: 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34
 [2ND COL]: 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35
 [3RD COL]: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36""")
+time.sleep(0.3)
 
-win = 29
-szam = input("Hova szeretnél tenni?")
-if 1 == 1:
-    if szam == 0:
-        r0 = "X"
-        win = 0
-    elif szam == 1:
-        r1 = "X"
-        win = 1
-    elif szam == 2:
-        r2 = "X"
-        win = 2
-    elif szam == 3:
-        r3 = "X"
-        win = 3
-    elif szam == 4:
-        r4 = "X"
-        win = 4
-    elif szam == 5:
-        r5 = "X"
-        win = 5
-    elif szam == 6:
-        r6 = "X"
-        win = 6
-    elif szam == 7:
-        r7 = "X"
-        win = 7
-    elif szam == 8:
-        r8 = "X"
-        win = 8
-    elif szam == 9:
-        r9 = "X"
-        win = 9
-    elif szam == 10:
-        r10 = "X"
-        win = 10
-    elif szam == 11:
-        r11 = "X"
-        win = 11
-    elif szam == 12:
-        r12 = "X"
-        win = 12
-    elif szam == 13:
-        r13 = "X"
-        win = 13
-    elif szam == 14:
-        r14 = "X"
-        win = 14
-    elif szam == 15:
-        r15 = "X"
-        win = 15
-    elif szam == 16:
-        r16 = "X"
-        win = 16
-    elif szam == 17:
-        r17 = "X"
-        win = 17
-    elif szam == 18:
-        r18 = "X"
-        win = 18
-    elif szam == 19:
-        r19 = "X"
-        win = 19
-    elif szam == 20:
-        r20 = "X"
-        win = 20
-    elif szam == 21:
-        r21 = "X"
-        win = 21
-    elif szam == 22:
-        r22 = "X"
-        win = 22
-    elif szam == 23:
-        r23 = "X"
-        win = 23
-    elif szam == 24:
-        r24 = "X"
-        win = 24
-    elif szam == 25:
-        r25 = "X"
-        win = 25
-    elif szam == 26:
-        r26 = "X"
-        win = 26
-    elif szam == 27:
-        r27 = "X"
-        win = 27
-    elif szam == 28:
-        r28 = "X"
-        win = 28
-    elif szam == 29:
-        r29 = "X"
-        win = 29
-    elif szam == 30:
-        r30 = "X"
-        win = 30
-    elif szam == 31:
-        r31 = "X"
-        win = 31
-    elif szam == 32:
-        r32 = "X"
-        win = 32
-    elif szam == 33:
-        r33 = "X"
-        win = 33
-    elif szam == 34:
-        r34 = "X"
-        win = 34
-    elif szam == 35:
-        r35 = "X"
-        win = 35
-    elif szam == 36:
-        r36 = "X"
-        win = 36
-    elif szam == "[1-18]":
-        r37 = "X"
-        win = 37
-    elif szam == "[19-36]":
-        r38 = "X"
-        win = 38
-    elif szam == "[EVEN]":
-        r39 = "X"
-        win = 39
-    elif szam == "[ODD]":
-        r40 = "X"
-        win = 40
-    elif szam == "[RED]":
-        r41 = "X"
-        win = 41
-    elif szam == "[BLACK]":
-        r42 = "X"
-        win = 42
-    elif szam == "[1ST 12]":
-        r43 = "X"
-        win = 43
-    elif szam == "[2ND 12]":
-        r44 = "X"
-        win = 44
-    elif szam == "[3RD 12]":
-        r45 = "X"
-        win = 45
-    elif szam == "[1ST COL]":
-        r46 = "X"
-        win = 46
-    elif szam == "[2ND COL]":
-        r47 = "X"
-        win = 47
-    elif szam == "[3RD COL]":
-        r48 = "X"
-        win = 48
+bet = int(input("Mennyit szeretnél rakni? "))
+
+while bet > egyenleg:
+    print("Nem tudsz ennyit rakni.")
+    bet = int(input("Mennyit szeretnél rakni? "))
+time.sleep(0.2)
+egyenleg -= bet
+szam = input("\nHova szeretnél tenni? ")
+
+os.system('cls')
+
+for i in range(1,4):
 
 
+    
+    print(f"Egyenleg: {egyenleg}")
+    print(f"""
+|--------------|
+|       {r0}      |
+|--------------|
+|  {r1} |  {r2} |  {r3} |
+|--------------|
+|  {r4} |  {r5} |  {r6} |
+|--------------|
+|  {r7} |  {r8} |  {r9} |
+|--------------|
+| {r10} | {r11} | {r12} |
+|--------------|
+| {r13} | {r14} | {r15} |
+|--------------|
+| {r16} | {r17} | {r18} |
+|--------------|
+| {r19} | {r20} | {r21} |
+|--------------|
+| {r22} | {r23} | {r24} |
+|--------------|
+| {r25} | {r26} | {r27} |
+|--------------|
+| {r28} | {r29} | {r30} |
+|--------------|
+| {r31} | {r32} | {r33} |
+|--------------|
+| {r34} | {r35} | {r36} |
+|--------------|
+
+{r37}
+{r38}
+{r39}
+{r40}
+{r41}
+{r42}
+{r43}
+{r44}
+{r45}
+{r46}
+{r47}
+{r48}
+""")
+    print(f"Sorsolas{"."*i}")
+    time.sleep(1)
+    os.system('cls')
 nyero = random.randint(0,36)
-print(nyero)
+
 if nyero == szam:
     egyenleg += (bet*36)
 elif (szam == "[1-18]" or szam == "[19-36]" or szam == "[EVEN]" or szam == "[ODD]" or szam == "[RED]" or szam == "[BLACK]") and (nyero in rr37 or nyero in rr38 or nyero in rr39 or nyero in rr40 or nyero in rr41 or nyero in rr42):
     egyenleg += (bet*2)
 elif (szam == "[1ST 12]" or szam == "[2ND 12]" or szam == "[3RD 12]" or szam == "[1ST COL]" or szam == "[2ND COL]" or szam == "[3RD COL]") and (nyero in rr43 or nyero in rr44 or nyero in rr45 or nyero in rr46 or nyero in rr47 or nyero in rr48):
     egyenleg += (bet*3)
-print(egyenleg)
 
+print(f"Egyenleg: {egyenleg}")
+print(f"""|--------------|
+|       {r0}      |
+|--------------|
+|  {r1} |  {r2} |  {r3} |
+|--------------|
+|  {r4} |  {r5} |  {r6} |
+|--------------|
+|  {r7} |  {r8} |  {r9} |
+|--------------|
+| {r10} | {r11} | {r12} |
+|--------------|
+| {r13} | {r14} | {r15} |
+|--------------|
+| {r16} | {r17} | {r18} |
+|--------------|
+| {r19} | {r20} | {r21} |
+|--------------|
+| {r22} | {r23} | {r24} |
+|--------------|
+| {r25} | {r26} | {r27} |
+|--------------|
+| {r28} | {r29} | {r30} |
+|--------------|
+| {r31} | {r32} | {r33} |
+|--------------|
+| {r34} | {r35} | {r36} |
+|--------------|
+
+{r37}
+{r38}
+{r39}
+{r40}
+{r41}
+{r42}
+{r43}
+{r44}
+{r45}
+{r46}
+{r47}
+{r48}
+""")
+print(f"A sorsolás erdménye: {nyero}")
